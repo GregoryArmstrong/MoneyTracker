30b153e7678a9e770d430aaa9e8f7628d9bcf1c6
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-01-10 15:35:49 -0700

    fixes the two broken tests related to nils

--- spec/features/registered_user_deletes_transaction_spec.rb
+++ spec/features/registered_user_deletes_transaction_spec.rb
@@ -6,6 +6,11 @@ RSpec.feature "User can delete a transaction" do
     user = User.create(username: "Greg", password: "password")
     transaction = Transaction.create(amount: 10, description: "pizza", category_id: category.id, user_id: user.id)
 
+    visit login_path
+    fill_in "Username", with: "Greg"
+    fill_in "Password", with: "password"
+    click_button "Login"
+
     visit category_transactions_path(category)
 
     click_on "Delete Transaction"
