715a8ba600ab1e177d35ecf69fb9c97ab2519b89
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-12-30 11:12:50 -0700

    Modify edit transaction form to dynamically set selected attribute on correct category option in category menu.

--- app/assets/javascripts/components/_user_show_transaction.js.jsx
+++ app/assets/javascripts/components/_user_show_transaction.js.jsx
@@ -1,4 +1,4 @@
-var Transaction = React.createClass({
+  var Transaction = React.createClass({
   getInitialState() {
     return { editable: false }
   },
@@ -19,6 +19,14 @@ var Transaction = React.createClass({
     this.setState({ editable: !this.state.editable })
   },
 
+  setSelectedAttribute(transactionCategoryID, optionCategoryID) {
+    if (transactionCategoryID == optionCategoryID) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
   render(){
     const categoryNames = {
                             1: 'Health Insurance',
@@ -32,27 +40,40 @@ var Transaction = React.createClass({
                             9: 'Utilities',
                             10: 'Phone'
                           };
+    var transactionDescription = this.props.transaction.description;
     var description   = this.state.editable ? <input type='text'
                                                    ref='description'
-                                                   defaultValue={ this.props.transaction.description } />
-                                            : <h1>{ this.props.transaction.description }</h1>;
+                                                   defaultValue={ transactionDescription } />
+                                            : <h1>{ transactionDescription }</h1>;
+    var transactionAmount = this.props.transaction.amount;
     var amount        = this.state.editable ? <input type='text'
                                               ref='amount'
-                                              defaultValue={ this.props.transaction.amount / 100 } />
-                                            : <h2 className='transaction-amount'>${ this.props.transaction.amount / 100 }</h2>;
+                                              defaultValue={ transactionAmount / 100 } />
+                                            : <h2 className='transaction-amount'>$ { transactionAmount / 100 }</h2>;
+    var transactionCategoryID = this.props.transaction.category_id;
     var categories    = this.state.editable ? (<select ref='editableTransactionCategory'>
-                                                <option value='1'>Health Insurance</option>
-                                                <option value='2'>Food</option>
-                                                <option value='3'>Car Payment</option>
-                                                <option value='4'>Car Insurance</option>
-                                                <option value='5'>Miscellaneous</option>
-                                                <option value='6'>Income</option>
-                                                <option value='7'>Rent</option>
-                                                <option value='8'>Loan Payment</option>
-                                                <option value='9'>Utilities</option>
-                                                <option value='10'>Phone</option>
+                                                <option value='1'
+                                                        selected={ this.setSelectedAttribute(transactionCategoryID, 1) }>Health Insurance</option>
+                                                <option value='2'
+                                                        selected={ this.setSelectedAttribute(transactionCategoryID, 2) }>Food</option>
+                                                <option value='3'
+                                                        selected={ this.setSelectedAttribute(transactionCategoryID, 3) }>Car Payment</option>
+                                                <option value='4'
+                                                        selected={ this.setSelectedAttribute(transactionCategoryID, 4) }>Car Insurance</option>
+                                                <option value='5'
+                                                        selected={ this.setSelectedAttribute(transactionCategoryID, 5) }>Miscellaneous</option>
+                                                <option value='6'
+                                                        selected={ this.setSelectedAttribute(transactionCategoryID, 6) }>Income</option>
+                                                <option value='7'
+                                                        selected={ this.setSelectedAttribute(transactionCategoryID, 7) }>Rent</option>
+                                                <option value='8'
+                                                        selected={ this.setSelectedAttribute(transactionCategoryID, 8) }>Loan Payment</option>
+                                                <option value='9'
+                                                        selected={ this.setSelectedAttribute(transactionCategoryID, 9) }>Utilities</option>
+                                                <option value='10'
+                                                        selected={ this.setSelectedAttribute(transactionCategoryID, 10) }>Phone</option>
                                              </select>)
-                                            : <h2> - { categoryNames[this.props.transaction.category_id] } - </h2>;
+                                            : <h2> - { categoryNames[transactionCategoryID] } - </h2>;
     var date          = this.state.editable ? <input ref='transactionDate' type='date' name='date' />
                                             : <h2>{ this.props.transaction.date }</h2>;
 
